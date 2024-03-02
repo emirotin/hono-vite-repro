f0ac3353ab37cfa1204639c784f1748465c00ee2
Eugene Mirotin <emirotin@gmail.com>
2024-03-03 00:57:28 +0200

    update demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
+import { useRef, useState } from "hono/jsx";
+
 export function App() {
-  return <h1>Test111221</h1>;
+  const a = useRef(false);
+  const [b, setB] = useState(false);
+
+  return (
+    <>
+      <p>A: {String(a.current)}</p>
+      <p>B: {String(b)}</p>
+      <button
+        onClick={() => {
+          a.current = true;
+          setB(true);
+        }}
+      >
+        Set B to true
+      </button>
+      {a.current && b && <p>OK</p>}
+    </>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ const app = new Hono();
 app.get("*", renderer);
 
 app.get("/", (c) => {
-  return c.render(<h1>Hello111!</h1>);
+  return c.render(<h1>Hi</h1>);
 });
 
 export default app;
