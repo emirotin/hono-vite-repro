30db7026f256fa159997ca9a2531565b37c6b91e
Eugene Mirotin <emirotin@gmail.com>
2024-03-03 21:29:31 +0200

    simulate issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,50 @@
-import { useRef, useState } from "hono/jsx";
+import { useEffect, useRef, useState } from "hono/jsx";
 
 export function App() {
-  const a = useRef(false);
-  const [b, setB] = useState(false);
+  const data = useRef<Uint8Array>(null);
+  const [done, setIsDone] = useState(false);
+
+  useEffect(() => {
+    console.log("effect", data.current?.length);
+  }, [data.current]);
+
+  const delay = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
+
+  const handleNewValue = (i: number) => {
+    const newLength = (data.current?.length ?? 0) + 1;
+    const newArray = new Uint8Array(newLength);
+    if (data.current) {
+      newArray.set(data.current);
+    }
+    newArray[newLength - 1] = i;
+    data.current = newArray;
+    console.log("debug", data.current.length);
+
+    if (data.current?.length === 100) {
+      setIsDone(true);
+    }
+  };
+
+  const start = () => {
+    const run = async () => {
+      for (let i = 0; i < 100; i++) {
+        handleNewValue(i);
+        await delay(10);
+      }
+    };
+
+    run();
+  };
+
+  if (done) {
+    console.log(data.current);
+    return <div>All done</div>;
+  }
 
   return (
     <>
-      <p>A: {String(a.current)}</p>
-      <p>B: {String(b)}</p>
-      <button
-        onClick={() => {
-          a.current = true;
-          setB(true);
-        }}
-      >
-        Set B to true
-      </button>
-      {a.current && b && <p>OK</p>}
+      <button onClick={start}>Start</button>
     </>
   );
 }
